736890ee71db83dbbc6fb2de20c5bf900fd927a7
johnrobertferrer <john.robert.s.ferrer@gmail.com>
2022-10-31 18:47:02 +0800

    :bug: fix: category filtering
    Signed-off-by: johnrobertferrer <john.robert.s.ferrer@gmail.com>

--- components/projects/ProjectsFilter.vue
+++ components/projects/ProjectsFilter.vue
@@ -9,9 +9,9 @@ export default {
       type: Array,
       default: () => [
         "Web Application",
-        "Mobile Application",
-        "UI/UX Design",
-        "Branding & Anim",
+        "Website",
+        "Content Management System",
+        "Internet of Things (IOT)",
       ],
     },
   },
